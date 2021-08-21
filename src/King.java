9993bcef20ecbc48f1949aa186f0d263bbab18a4
jake <jake.arsenault@hotmail.ca>
2021-08-21 13:28:50 -0400

    Determine directory path based on OS.  Compatible with Windows/Linux based OSes

--- src/King.java
+++ src/King.java
@@ -14,10 +14,10 @@ public class King extends Piece{
 		this.moves = new ArrayList<Integer>();
 		
 		if(this.getColour()==-1) {
-			this.icon = new Image(new FileInputStream(System.getProperty("user.dir")+"/ChessPieces/whiteKing.png"));
+			this.icon = new Image(new FileInputStream(Main.DIR_PREFIX+"whiteKing.png"));
 		}
 		else {
-			this.icon = new Image(new FileInputStream(System.getProperty("user.dir")+"/ChessPieces/blackKing.png"));
+			this.icon = new Image(new FileInputStream(Main.DIR_PREFIX+"blackKing.png"));
 		}
 	}
 	
